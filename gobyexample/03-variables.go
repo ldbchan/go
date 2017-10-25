087bafb6e07a6a98f4eebe19e18ec06adf201d26
ldbchan <ldb.chan@gmail.com>
2017-10-25 23:14:42 +0800

    Add gobyexample files 01-06

---
+++ gobyexample/03-variables.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    var a string = "initial"
+    fmt.Println(a)
+
+    var b, c int = 1, 2
+    fmt.Println(b, c)
+
+    var d = true
+    fmt.Println(d)
+
+    // int var is initialized as 0
+    var e int
+    fmt.Println(e)
+
+    // shorthand for 'var f string = "short"' 
+    f := "short"
+    fmt.Println(f)
+
+    var g string = "short"
+    fmt.Println(g)
+}
---
+++ gobyexample/05-for.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    // output
+    // 1
+    // 2
+    // 3
+    i := 1
+    for i <= 3 {
+        fmt.Println(i)
+        i = i + 1 // `i += 1` is also valid
+    }
+
+    // output
+    // 7
+    // 8
+    // 9
+    for j := 7; j <= 9; j++ {
+        fmt.Println(j)
+    }
+
+    // output
+    // loop
+    for {
+        fmt.Println("loop")
+        break
+    }
+
+    // output
+    // 1
+    // 3
+    // 5
+    for n := 0; n <= 5; n++ {
+        if n%2 == 0 {
+            continue
+        }
+        fmt.Println(n)
+    }
+}
